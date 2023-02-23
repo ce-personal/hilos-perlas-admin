44e294de29598282235553308acdc7053e411203
Carlos Espinoza <cespinoza@plannerapp.net>
2023-02-23 07:15:55 -0600

    Subiendo con el servidor correcto

--- src/__mocks__/env.js
+++ src/__mocks__/env.js
@@ -1,6 +1,6 @@
 const env = {
     API_URL: 'https://de-hilos-y-perlas.azurewebsites.net',
-    API_URL: 'https://734e-143-202-253-244.ngrok.io',
+    // API_URL: 'https://734e-143-202-253-244.ngrok.io',
     // API_URL: 'https://localhost:44368',
 };
 
