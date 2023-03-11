39777124c65858d1d742e9e3de594daf96506a26
Carlos Espinoza <cespinoza@plannerapp.net>
2023-03-10 22:27:02 -0600

    Se hicieron cambios

--- src/__mocks__/env.js
+++ src/__mocks__/env.js
@@ -1,7 +1,7 @@
 const env = {
-    // API_URL: 'https://de-hilos-y-perlas.azurewebsites.net',
+    API_URL: 'https://de-hilos-y-perlas.azurewebsites.net',
     // API_URL: 'https://734e-143-202-253-244.ngrok.io',
-    API_URL: 'https://localhost:44368',
+    // API_URL: 'https://localhost:44368',
 };
 
 export default env;
